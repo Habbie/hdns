d9f40b44786bff225141265edf5545617e69b88d
Peter van Dijk <peter@7bits.nl>
2018-04-29 22:38:25 +0200

    fully parse query

--- main.lua
+++ main.lua
@@ -1,6 +1,8 @@
 #!/usr/bin/env luajit
 local socket = require'socket'
 local bit = require'bit'
+local inspect = require'inspect'
+
 local sock = socket.udp()
 sock:setsockname('*', 5300)
 
@@ -14,11 +16,20 @@ local function bytereader(data)
 		offset = offset + 1
 		return data:byte(offset -1)
 	end
+	local function getstring(n)
+		print("#data", #data)
+		print("offset, n, offset + n -1", offset, n, offset+n -1)
+		assert(#data >= offset + n -1, "not enough bytes for getstring")
+		local ret = data:sub(offset, offset + n -1)
+		offset = offset + n
+		return ret
+	end
 	local function getword()
 		return getbyte() * 256 + getbyte()
 	end
 	return {
 		getbyte = getbyte,
+		getstring = getstring,
 		getword = getword
 	}
 end
@@ -31,13 +42,10 @@ local function bitreader(data, size)
 		local res = 0
 		local resoffset = n-1
 		while resoffset >= 0 do
-			assert("ran out of bits", offset>=0)
-			print("offset", offset)
+			assert(offset>=0, "ran out of bits")
 			local mask = bit.lshift(1, offset)
 			local maskedbit = bit.band(data, mask)
-			print("maskedbit", maskedbit)
 			local maskedbitshifted = bit.rshift(maskedbit, offset)
-			print("maskedbitshifted", maskedbitshifted)
 			res = bit.bor(res, bit.lshift(maskedbitshifted, resoffset))
 			resoffset = resoffset - 1
 			offset = offset - 1
@@ -48,6 +56,28 @@ local function bitreader(data, size)
 		getbits = getbits
 	}
 end
+
+local function readname(byr)
+	local namelen = 0
+	local res = {}
+	while true do
+		local labellen = byr.getbyte()
+		-- print("labellen", labellen)
+		if labellen == 0 then
+			break
+		end
+		-- FIXME handle compression
+		assert(labellen <= 63, "label length over 63")
+		local label = byr.getstring(labellen)
+		-- print("label", label)
+		table.insert(res, 1, label)
+		namelen = namelen + 1 + labellen
+		assert(namelen < 255, "name too long")
+	end
+	return res
+end
+
+
 
 local byr = bytereader(data)
 req.id = byr.getword()
@@ -70,4 +100,37 @@ req.ancount = byr.getword()
 req.nscount = byr.getword()
 req.arcount = byr.getword()
 
-for k,v in pairs(req) do print(k,v) end
+req.qd = {}
+
+for i=1, req.qdcount do
+	table.insert(req.qd, {
+		qname = readname(byr),
+		qtype = byr.getword(),
+		qclass = byr.getword()
+	})
+end
+
+local function getrrs(byr, count)
+	local ret = {}
+	for i=1, count do
+		local _ret = {
+			qname = readname(byr),
+			qtype = byr.getword(),
+			qclass = byr.getword(),
+			ttl = byr.getword() * 65536 + byr.getword(),
+			rdlength = byr.getword()
+		}
+		_ret.rdata = byr.getstring(_ret.rdlength)
+		print("#rdata", #_ret.rdata)
+		table.insert(ret, _ret)
+	end
+	return ret
+end
+
+req.an = getrrs(byr, req.ancount)
+req.ns= getrrs(byr, req.nscount)
+req.ar = getrrs(byr, req.arcount)
+
+print(inspect(req))
+
+
