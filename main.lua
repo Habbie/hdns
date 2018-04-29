dae5269cccfc4f40099d0122d5c615f9c234c1d7
Peter van Dijk <peter@7bits.nl>
2018-04-29 22:43:47 +0200

    spaces, not tabs

--- main.lua
+++ main.lua
@@ -11,70 +11,70 @@ print(type(data))
 
 local req = {}
 local function bytereader(data)
-	local offset = 1
-	local function getbyte()
-		offset = offset + 1
-		return data:byte(offset -1)
-	end
-	local function getstring(n)
-		print("#data", #data)
-		print("offset, n, offset + n -1", offset, n, offset+n -1)
-		assert(#data >= offset + n -1, "not enough bytes for getstring")
-		local ret = data:sub(offset, offset + n -1)
-		offset = offset + n
-		return ret
-	end
-	local function getword()
-		return getbyte() * 256 + getbyte()
-	end
-	return {
-		getbyte = getbyte,
-		getstring = getstring,
-		getword = getword
-	}
+    local offset = 1
+    local function getbyte()
+        offset = offset + 1
+        return data:byte(offset -1)
+    end
+    local function getstring(n)
+        print("#data", #data)
+        print("offset, n, offset + n -1", offset, n, offset+n -1)
+        assert(#data >= offset + n -1, "not enough bytes for getstring")
+        local ret = data:sub(offset, offset + n -1)
+        offset = offset + n
+        return ret
+    end
+    local function getword()
+        return getbyte() * 256 + getbyte()
+    end
+    return {
+        getbyte = getbyte,
+        getstring = getstring,
+        getword = getword
+    }
 end
 
 local function bitreader(data, size)
-	-- size is a multiple of 8
-	print("bitreader data", data, size)
-	local offset = size - 1
-	local function getbits(n)
-		local res = 0
-		local resoffset = n-1
-		while resoffset >= 0 do
-			assert(offset>=0, "ran out of bits")
-			local mask = bit.lshift(1, offset)
-			local maskedbit = bit.band(data, mask)
-			local maskedbitshifted = bit.rshift(maskedbit, offset)
-			res = bit.bor(res, bit.lshift(maskedbitshifted, resoffset))
-			resoffset = resoffset - 1
-			offset = offset - 1
-		end
-		return res
-	end
-	return {
-		getbits = getbits
-	}
+    -- size is a multiple of 8
+    print("bitreader data", data, size)
+    local offset = size - 1
+    local function getbits(n)
+        local res = 0
+        local resoffset = n-1
+        while resoffset >= 0 do
+            assert(offset>=0, "ran out of bits")
+            local mask = bit.lshift(1, offset)
+            local maskedbit = bit.band(data, mask)
+            local maskedbitshifted = bit.rshift(maskedbit, offset)
+            res = bit.bor(res, bit.lshift(maskedbitshifted, resoffset))
+            resoffset = resoffset - 1
+            offset = offset - 1
+        end
+        return res
+    end
+    return {
+        getbits = getbits
+    }
 end
 
 local function readname(byr)
-	local namelen = 0
-	local res = {}
-	while true do
-		local labellen = byr.getbyte()
-		-- print("labellen", labellen)
-		if labellen == 0 then
-			break
-		end
-		-- FIXME handle compression
-		assert(labellen <= 63, "label length over 63")
-		local label = byr.getstring(labellen)
-		-- print("label", label)
-		table.insert(res, 1, label)
-		namelen = namelen + 1 + labellen
-		assert(namelen < 255, "name too long")
-	end
-	return res
+    local namelen = 0
+    local res = {}
+    while true do
+        local labellen = byr.getbyte()
+        -- print("labellen", labellen)
+        if labellen == 0 then
+            break
+        end
+        -- FIXME handle compression
+        assert(labellen <= 63, "label length over 63")
+        local label = byr.getstring(labellen)
+        -- print("label", label)
+        table.insert(res, 1, label)
+        namelen = namelen + 1 + labellen
+        assert(namelen < 255, "name too long")
+    end
+    return res
 end
 
 
@@ -103,28 +103,28 @@ req.arcount = byr.getword()
 req.qd = {}
 
 for i=1, req.qdcount do
-	table.insert(req.qd, {
-		qname = readname(byr),
-		qtype = byr.getword(),
-		qclass = byr.getword()
-	})
+    table.insert(req.qd, {
+        qname = readname(byr),
+        qtype = byr.getword(),
+        qclass = byr.getword()
+    })
 end
 
 local function getrrs(byr, count)
-	local ret = {}
-	for i=1, count do
-		local _ret = {
-			qname = readname(byr),
-			qtype = byr.getword(),
-			qclass = byr.getword(),
-			ttl = byr.getword() * 65536 + byr.getword(),
-			rdlength = byr.getword()
-		}
-		_ret.rdata = byr.getstring(_ret.rdlength)
-		print("#rdata", #_ret.rdata)
-		table.insert(ret, _ret)
-	end
-	return ret
+    local ret = {}
+    for i=1, count do
+        local _ret = {
+            qname = readname(byr),
+            qtype = byr.getword(),
+            qclass = byr.getword(),
+            ttl = byr.getword() * 65536 + byr.getword(),
+            rdlength = byr.getword()
+        }
+        _ret.rdata = byr.getstring(_ret.rdlength)
+        print("#rdata", #_ret.rdata)
+        table.insert(ret, _ret)
+    end
+    return ret
 end
 
 req.an = getrrs(byr, req.ancount)
